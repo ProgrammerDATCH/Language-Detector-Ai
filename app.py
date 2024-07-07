5827f16d4589e81b536597f6141a25aec744f6db
ProgrammerDATCH <ProgrammerDATCH@gmail.com>
2024-07-07 10:40:12 +0200

    req-fix

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ import streamlit as st
 import webbrowser
 import os
 
+st.title("Language Detection AI")
+
 global Lrdetect_Model
 
+# Check if the file exists
 if os.path.exists('language_detection_model.pckl'):
     with open('language_detection_model.pckl', 'rb') as LrdetectFile:
         try:
@@ -15,14 +18,15 @@ if os.path.exists('language_detection_model.pckl'):
 else:
     st.error("File not found: language_detection_model.pckl")
 
-LrdetectFile = open('language_detection_model.pckl', 'rb')
-Lrdetect_Model = pickle.load(LrdetectFile)
-LrdetectFile.close()
-st.title("Language Detection Ai")
 input_test = st.text_input("Enter text to detect language", "amakuru yawe")
 
 button_clicked = st.button("Get Language Name")
 if button_clicked:
-    st.text(Lrdetect_Model.predict([input_test]))
+    try:
+        prediction = Lrdetect_Model.predict([input_test])
+        st.text(prediction)
+    except Exception as e:
+        st.error(f"Error making prediction: {e}")
+
 st.markdown("---")
 st.markdown("Created by [ProgrammerDATCH](https://programmerdatch.netlify.app)", unsafe_allow_html=True)
