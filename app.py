b3063c1eda12474e41176f3c6dce77e98b4e31dc
ProgrammerDATCH <ProgrammerDATCH@gmail.com>
2024-07-07 10:37:38 +0200

    req-fix

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ import pickle
 import string
 import streamlit as st
 import webbrowser
+import os
 
 global Lrdetect_Model
+
+if os.path.exists('language_detection_model.pckl'):
+    with open('language_detection_model.pckl', 'rb') as LrdetectFile:
+        try:
+            Lrdetect_Model = pickle.load(LrdetectFile)
+        except Exception as e:
+            st.error(f"Error loading model: {e}")
+else:
+    st.error("File not found: language_detection_model.pckl")
 
 LrdetectFile = open('language_detection_model.pckl', 'rb')
 Lrdetect_Model = pickle.load(LrdetectFile)
